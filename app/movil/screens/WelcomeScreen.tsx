0854ebf7b2307592cf07a1bcde0af6ed4f74d95b
Sebastian <sherrera@usm.cl>
2025-09-01 15:56:43 -0400

    ultimos cambios

--- app/movil/screens/WelcomeScreen.tsx
+++ app/movil/screens/WelcomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, ImageBackground, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
 
 const WelcomeScreen: React.FC = () => {
@@ -16,12 +16,14 @@ const WelcomeScreen: React.FC = () => {
       resizeMode="cover"
     >
       <View style={styles.container}>
-        <Text style={styles.title}>Bienvenido</Text>
-        <Text style={styles.description}>
-          SegurUp es una app diseñada para proteger a la comunidad. 
-          Participa, reporta y colabora. Juntos hacemos la ciudad más segura.
-        </Text>
-       
+        <View style={styles.topContainer}>
+          <Text style={styles.title}>Bienvenido</Text>
+          <Text style={styles.description}>
+            SegurUp es una app diseñada para proteger a la comunidad.{'\n'}
+            Participa, reporta y colabora..{'\n'} 
+            Juntos hacemos la ciudad más segura.
+          </Text>
+        </View>
         <TouchableOpacity style={styles.button} onPress={handleStart}>
           <Text style={styles.buttonText}>Comencemos! →</Text>
         </TouchableOpacity>
@@ -37,31 +39,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'rgba(10, 31, 46, 0.8)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'space-between', // Espacia el contenido entre la parte superior y inferior
     padding: 20,
   },
-  topText: {
-    position: 'absolute',
-    top: 40,
-    color: '#FFFFFF',
-    fontSize: 14,
+  topContainer: {
+    alignItems: 'center',
   },
   title: {
     color: '#FFFFFF',
     fontSize: 28,
     fontWeight: 'bold',
-    marginBottom: 10,
+    marginTop: 60,
+    marginBottom: 20,
   },
   description: {
     color: '#FFFFFF',
     fontSize: 16,
     textAlign: 'center',
-    marginBottom: 40,
-  },
-  logo: {
-    width: 120,
-    height: 120,
+    marginHorizontal: 25,
     marginBottom: 40,
   },
   button: {
@@ -69,6 +64,8 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     paddingHorizontal: 24,
     borderRadius: 25,
+    alignSelf: 'center', // Centra el botón horizontalmente
+    marginBottom: 20, // Margen inferior para simular la posición en la imagen
   },
   buttonText: {
     color: '#FFFFFF',
